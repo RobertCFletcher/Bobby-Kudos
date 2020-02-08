bd93d7783560bca0274f92a33112c493158bee15
Daniel <yoppd@oregonstate.edu>
2020-02-08 18:46:18 -0500

    API authentication for manager tied in Admin authentication still none functional.
    Populated manager page with temp data via ejs
    
    create account valid to create manager accouts, admin accounts not available.

--- server.js
+++ server.js
@@ -1,30 +1,151 @@
    //using nodemon to auto restart server
    //commandline: "npm run devstart" to run server on auto restart
-   
-   var users = []        //used for storing data and debugging locally, will replace with API calls to DB
-   
-   //May want to keep key in environment vars file for secret key; NOT CURRENTLY IN USE
-//    if (process.env.NODE_ENV !== "production" ){
-//        require("dotenv").config()
-//    }
 
+   //USER API FORMAT:
+   // users.id ; users.email ; users.hash ; users.type
 
+   //INCLUDED PACKAGES
+   //==================================================================
    var express = require("express"),
         bodyParser = require("body-parser"),
         bcrypt = require("bcrypt"),
         session = require('express-session'),
         passport = require("passport"),
         flash = require("express-flash");
+    var LocalStrategy = require("passport-local").Strategy;         //Just need strategy from passport local
+    var bcrypt = require("bcrypt");
+    var request = require("request");                                     //used to hash/salt passwords and compare password hashes
     var app = express();
 
-    var initializePassport = require("./passport-config");  //Moved passport setup into passport-config.js   
-    initializePassport(                                     //Initilize passport setup
-        passport, 
-                                                            //REPLACE WITH API CALLS:
-        email => users.find(user => user.email === email),  //Getuserbyemail, returns user hash, id, and account type based on email
-        id => users.find(user => user.id === id )           //GetuserbyID, returns user hash, email, and account type based on id
-    );
+    // PASSPORT INITIALIZATION FUNCTION
+    //  Authentication function 
+    //  Take user email and password as params 
+    //  Get user info from database using user entered email as key, 
+    //  Compare user entered password with stored user password hash
+    //=================================================================
+    function initializePassport(passport, getUserByEmail, getUserById){
+        var authenticateUser = async function(email, password, done){
+                try{
+                    var user = await getUserByEmail(email)                      //await is expecting a promise to be resolved in getUserByEmail
+// /*DEBUG*/           console.log("||| DEBUG 2 (start auth) - this is the user", user.id, user.hash, user.email, user.type)
+                    testUser(user, password, done)
+                   } 
+                catch(error){
+                    return done(error, false, )                                          //some authentication error
+                   }
+        }
+        passport.use(new LocalStrategy({ usernameField: 'userEmail', passwordField: "userPass"}, authenticateUser))
+        passport.serializeUser(function(user, done){
+              done(null, {id: user.id, type: user.type });                      //Store both ID and type
+              })
+              //saves the user.id in the session
+        passport.deserializeUser(async (id, done) =>  {
+             var getUser = await getUserById(id) 
+             return done(null, getUser)    
+        })  
 
+    }
+    //================================================================    
+    function testUser( user, password, done){
+
+                if(user==null){
+                   //No matching email in DB to user's email
+                   return done(null, false, { message: "No user with that email" })
+                }
+                if(bcrypt.compare(password, user.password)){
+                   //password correct, return authenticated user
+                   return done(null, user)
+                }
+                else {
+                    //password incorrect
+                    return done(null, false, { message: "Password is incorrect" })
+                }
+          
+    }
+    
+    //CALLING PASSPORT INITIALIZATION
+    //==================================================================    
+    initializePassport(                                       //Params: passport, getUserByEmail(), getUserByID()
+        passport, 
+        //GET USER BY EMAIL (API CALL)
+        function (email) { 
+                console.log("Get user by email", email)                                          //Consider encrypting email
+                return new Promise((resolve, reject) => {                   //PROMISE resolves to await in authenticateUser
+                coreAPI = "https://kudosapi.wl.r.appspot.com/users/";       //USER OBJECT format should be:
+                console.log(coreAPI + email)                                      //USER {id: "<number.", email: "<a@a.com>", hash: "<asdasd>", type: "admin/manager"}                    
+                request(coreAPI + email, function (error, response, body) 
+                {
+                    if (error) 
+                    {
+                        console.log(error);
+                        var user = null
+                        resolve(user)
+                    }
+                    if (!error) 
+                    {
+                        var personObj = JSON.parse(body);
+                        console.log(personObj);
+                        var user = {};
+                        user.id= personObj.userid;
+                        user.email = personObj.email;
+                        user.password = personObj.password;
+                        if(personObj.usertype === 1)
+                        {user.type = "admin"}
+                        else if(personObj.usertype === 2)
+                        {user.type = "manager"}
+                        else
+                        {user.type = null}
+                        user.createdby = personObj.createdby;
+                        console.log(user)
+                        resolve(user);
+                    }
+                });
+                
+            }) //end of promise
+        },
+        //GET USER BY ID (API CALL)                                                    
+        function (ID) { 
+                                                                                                //Consider encrypting ID
+                return new Promise((resolve, reject) => {                                           //PROMISE resolves to await in authenticateUser
+                    coreAPI = "https://kudosapi.wl.r.appspot.com/users";      //USER OBJECT format should be:
+                    //USER {id: "<number.", email: "<a@a.com>", hash: "<asdasd>", type: "admin/manager"}                    
+                    request(coreAPI, function (error, response, body) 
+                    {
+                        if(!error){
+                            // console.log("GET USER BY ID")
+                            // console.log(body);  //get all users
+                            var parsed = JSON.parse(body);
+                            // console.log(parsed); //parse from JSON to JS object 
+                            // console.log("ID: ", ID)
+                            var personObj = parsed.find(x => x.userid === ID.id) //Find object of user who matches ID
+                            // console.log("DEBUG",personObj,"DEBUG");
+                            var user = {};  //import found user info to new object
+                            user.id = personObj.userid;
+                            user.email = personObj.email;
+                            user.password = personObj.password;
+                            if(personObj.usertype === 1)
+                            {user.type = "admin"}
+                            else if(personObj.usertype === 2)
+                            {user.type = "manager"}
+                            else
+                            {user.type = null}
+                            user.createdby = personObj.createdby;
+                            // console.log(user)
+                            resolve(user);
+                            }
+                        else{
+                            console.log("ERROR- get user by Id")
+                            var user = null
+                            resolve(user);
+                            }
+                    });
+            })//end of promise
+        }
+    );
+        
+
+    //OTHER PACKAGE SETUP
+    //==================================================================
     app.set("view engine", "ejs");                          //establish ejs as template engine
     app.use(bodyParser.urlencoded({extended: true}));       //pull HTML-form vars into server.js; allow nested objects
     app.use(flash());                                       //express-flash is used by passport to return login error msgs to EJS template
@@ -35,73 +156,175 @@
     }))
     app.use(passport.initialize());
     app.use(passport.session());    //link passport to express sessions for persistant login
+    app.use(express.static("public")); //use public folder to hold assets
 
-//function to check if user currently has authentication cookie, if not redirect to login    
-function checkAuthenticated(req, res, next){
-    if (req.isAuthenticated()){
-        return next()
-    }
-    else{
-        res.redirect("/login");
-    }
-}
-
-
-//================ROUTES=============================== 
-
-// LOGIN PAGE   
-    app.get("/login", function(req, res){
-        res.render("login");
-    });
-
-    app.post("/login", passport.authenticate("local",{
-        // console.log(req.body.userEmail); console.log(req.body.userPass);
-        successRedirect: "/secret",
-        failureRedirect: "/login",
-        failureFlash: true      //displays flash message about failure type
-
-    }));
-
-
-// CREATE PAGE   
-    app.get("/create", function(req, res){
-        res.render("create");
-    });
-
-    app.post("/create", async(req,res) => {
-        // "accountType" is {"admin" or "manager"}
-        // console.log(req.body.userEmail); console.log(req.body.userPass); console.log(req.body.accountType);
-      
-        try{
-            var hashedPass = await bcrypt.hash(req.body.userPass, 10)       //Hash the user created password for safe storage (10 is Salt Val)
-            // API CALL HERE TO CREATE ACCOUNT
-            users.push({
-                id:     Math.floor( Math.random()*1000000000 ),    //Figure out a better way to give users unique IDs or just let DB do it
-                email: req.body.userEmail,
-                password: hashedPass,
-                type: req.body.accountType 
-            });
-            //should really check that account doesn't already exist. 
-            res.redirect("/create");
-        } 
-        catch(error) {
-            res.redirect("/badrequest")
+    //COOKIE CHECK
+    //==================================================================  
+    function checkAuthenticated(req, res, next){
+        if (req.isAuthenticated()){                 //Passport Auth function
+//            console.log("|||DEBUG 7 - Auth check");
+            return next()
         }
-        console.log(users);
+        else{
+//            console.log("|||DEBUG 8 - Auth failed")
+            res.redirect("/login");
+        }
+    }
+
+    //CHECK USER TYPE
+    //=================================================================
+    function requireRole(role){
+    return function (req, res, next) {
+        if (req.user && req.user.type === role) {
+//           console.log("|||DEBUG 5  - role check");
+            next();
+        } else {
+            res.redirect("./login");
+//            console.log("|||DEBUG 6 - Role Check Failure:", req.user, "    Role: ",role);
+        }
+      }
+    }
+
+
+// ROUTES
+//=============================================== 
+
+    // HOME REDIRECT
+        app.get("/", function(req,res){
+            res.redirect("/login")
+        });
+
+    // LOGIN PAGE   
+        app.get("/login", function(req, res){
+            res.render("login");
+        });
+
+        app.post("/login", passport.authenticate("local",{
+            // console.log(req.body.userEmail); console.log(req.body.userPass);
+            successRedirect: "/loginSuccess",
+            failureRedirect: "/login",
+            failureFlash: true      //displays flash message about failure type
+
+        }));
+
+    // LOGIN SUCCESS
+        app.get("/loginSuccess", checkAuthenticated, function(req,res){
+            if(req.user.type==="admin")
+            {res.redirect("/admin")}
+            else if(req.user.type==="manager")
+            {res.redirect("/manager")}
+            else
+            {res.send("412 - Precoditions Failed")}
+        });
+
+    // CREATE PAGE   
+        app.get("/create", function(req, res){
+            res.render("create");
+        });
+
+        app.post("/create", async(req,res) => {
+            // "accountType" is {"admin" or "manager"}
+            // console.log(req.body.userEmail); console.log(req.body.userPass); console.log(req.body.accountType);
         
-    });
+            try{
+                var hashedPass = await bcrypt.hash(req.body.userPass, 10)       //Hash the user created password for safe storage (10 is Salt Val)
+                // API CALL HERE TO CREATE ACCOUNT
+                console.log(req.body)
+                if(req.body.accountType === "manager")
+                {
+                        var data = {
+                        firstname: req.body.userFirst,
+                        lastname: req.body.userLast,
+                        email: req.body.userEmail,
+                        password: hashedPass,
+                        createdby: 1
+                        }
+                        // console.log(data)
+                        // var bodySub = JSON.stringify(data)
+                        // console.log(formdata)
 
-// SECRET PAGE   
-    app.get("/secret", checkAuthenticated, function(req, res){
-        //This is just a hidden page to test authentication but where we can add more API calls and create pages of user specific data
-        res.render("secret");
-    });
+                        var options = { method: 'POST',
+                          url: 'https://kudosapi.wl.r.appspot.com/users/managers',
+                          headers: 
+                           { 'cache-control': 'no-cache',
+                             'content-type': 'application/x-www-form-urlencoded' },
+                          form: false, 
+                          body: JSON.stringify(data)  
+                        };
+                        
+                        request(options, function (error, response, body) {
+                          if (error) throw new Error(error);
+                            
+                        //   console.log("Body", body);
+                        //   console.log("REsponse:", response)
+                          res.redirect("/create")
+                        });
+                }
+            } 
+            catch(error) {
+                res.redirect("/badrequest2")
+            }
+//            console.log(users);
+            
+        });
+
+    // ADMIN PAGE   
+        app.get("/admin", checkAuthenticated, requireRole("admin"), function(req, res){
+            res.render("admin.ejs");
+        });
+
+    // MANAGER PAGE
+        app.get("/manager", checkAuthenticated, requireRole("manager"), function(req, res){
+            tempData = [
+                {id: "ID1",
+                 timestamp: {time: "01-01-01"},
+                 region: "Region 1",
+                 type: "Most Productive",
+                 recipientname: "Morgan Freeman"
+                },
+                {id: "ID2",
+                 timestamp: {time: "02-02-02"},
+                 region: "Region 2",
+                 type: "Least Productive",
+                 recipientname: "Willis Freeman"
+                },
+                {id: "ID3",
+                 timestamp: {time: "03-03-03"},
+                 region: "Region 3",
+                 type: "Middlist Productive",
+                 recipientname: "Sarah Freeman"
+                }
+            ]
+            res.render("manager.ejs",{awardData: tempData});
+        });
+
+    //OTHER - gets user by ID
+        app.get("/other", function(req,res){
+            request("https://kudosapi.wl.r.appspot.com/users/bf@g.com", function(error, responce, body){
+                if(!error){
+                    console.log(body)
+                    var parsed = JSON.parse(body)
+                    console.log(parsed);
+                    res.redirect("/badrequest");
+                }
+                else{
+                    console.log("ERROR- get user by Id")
+                    res.render("/badrequest");
+                }
+            });
+        })    
+    //LOGOUT
+        app.get("/logout", function(req,res){
+            req.logout();
+            res.redirect("/login")
+        });
 
 
-// 404    
-    app.get("/*", function(req,res){
-        res.send("Request not Found! 404 Bro")
-    });
+
+    // 404    
+        app.get("/*", function(req,res){
+            res.send("Request not Found! 404 Bro")
+        });
 
 
 //================LISTEN===============================
